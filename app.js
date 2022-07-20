e573ab0dd86ad2bac6d411a9b8115e23a666a76c
Jakub <j.solnicka@seznam.cz>
2022-07-20 16:05:30 +0200

    1.00.04

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3333;
+const port = process.env.PORT || 3333;
 const compression = require("compression");
 const path = require("path");
 const bodyParser = require("body-parser");
